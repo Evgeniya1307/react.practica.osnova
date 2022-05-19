6558549cd036e9e2b78b1b108bc856c1f42ffe0b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-19 13:44:47 +0400

    practica react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Справочник Телефонный
         </p>
         <a
           className="App-link"
@@ -15,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+    Справочник Телефонный
         </a>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
