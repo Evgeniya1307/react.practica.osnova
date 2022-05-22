74a1025d4658a8c6d5213d78e6250723395b2368
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-22 22:26:03 +0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import Ccomponent from './Ccomponent';
-
-
+import Ccomponent from "./Ccomponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Ccomponent /> 
-     
-       
+        <Ccomponent />
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Справочник Телефонный
-        </p>
+        <p>Справочник Телефонный</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-    Справочник Телефонный
+          Справочник Телефонный
         </a>
       </header>
     </div>
-  );  
+  );
 }
 
 export default App;
--- src/Fcomponent.js
+++ src/Fcomponent.js
@@ -1,21 +1,21 @@
-// функциогнальная с хуком по счётчикам
+функциогнальная с хуком по счётчикам
 
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 
 
-// function Fcomponent() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <div>
-//   <p>Current:{count} </p> 
-//   <button onClick={() => setCount(0)}>Сбросить</button>
-//       <button onClick={() => setCount(count +1)}>-</button>
-//       <button onClick={() => setCount(count -1)}>-</button>
-//       </div>
-//   );
-// }
+function Fcomponent() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+  <p>Current:{count} </p> 
+  <button onClick={() => setCount(0)}>Сбросить</button>
+      <button onClick={() => setCount(count +1)}>-</button>
+      <button onClick={() => setCount(count -1)}>-</button>
+      </div>
+  );
+}
 
-// export default Fcomponent;
+export default Fcomponent;
 
 
 
